a93ce057fae6111009864e7a95b45bcc4cf75b70
larazayec <lara.zayec@gmail.com>
2022-06-21 17:14:47 +0300

    Creation History Page

---
+++ Test2/Pages/HistoryPage.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2.Pages
+{
+    internal class HistoryPage
+    {
+        private IWebDriver driver;
+        public HistoryPage(IWebDriver webDriver)
+        {
+            driver = webDriver;
+        }
+        public IWebElement NameElement => driver.FindElement(By.XPath("//table/tr[@class='data - th']"));
+
+        public List<string> HistoryFirst
+        {
+            get
+            {
+                List<string> actuale = new List<string>();
+                SelectElement historySelect = new SelectElement(NameElement);
+                foreach (IWebElement element in historySelect.Options)
+                {
+                    actuale.Add(element.Text);
+                }
+                return actuale;
+            }
+
+        }
+    }
+}
---
+++ Test2/Tests/HistoryPageTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test2.Pages;
+
+namespace Test2.Tests
+{
+    internal class HistoryPageTests : BaseTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            OpenDriver();
+            driver.Url = "https://localhost:5001/History";
+        }
+
+        [Test]
+        public void ClearHistory()
+        {
+            HistoryPage historyPage = new HistoryPage(driver);
+            IWebElement clearButton = driver.FindElement(By.Id("clear"));
+            List<string> expected = new List<string>(7) { "Amount", "%", "Term", "Year", "From", "To", "Interest", "Income"};
+            CalculatorPage calculatorPage = new CalculatorPage(driver);
+            Assert.AreEqual(expected, historyPage.HistoryFirst);
+
+
+            /*IWebElement ferst = driver.FindElement(By.XPath("//table/tr/td[1]"));
+            Assert.AreEqual("100.00", ferst.Text);
+            IWebElement ferst2 = driver.FindElement(By.XPath("//table/tr/td[2]"));
+            Assert.AreEqual("100%", ferst2.Text);
+            //IWebDriver arrayNames = driver.FindElement(By.XPath("//tr[@class = 'data - th']"));
+            clearButton.Click();*/
+
+        }
+
+    }
+}
