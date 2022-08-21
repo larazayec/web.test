3bdd58e75aea760da53ea77566f586662f20cd05
larazayec <lara.zayec@gmail.com>
2022-08-21 22:47:22 +0300

    PositivTest Registration

---
+++ Test2/Pages/RegisterPage.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2.Pages
+{
+    internal class RegisterPage
+    {
+        private IWebDriver driver;
+        public RegisterPage(IWebDriver webDriver)
+        {
+            driver = webDriver;
+        }
+        public IWebElement LoginField => driver.FindElement(By.Id("login"));
+        public IWebElement EmailField => driver.FindElement(By.Id("email"));
+        public IWebElement PasswordField => driver.FindElement(By.Id("password1"));
+        public IWebElement ConfirmField => driver.FindElement(By.Id("password2"));
+        public IWebElement RegisterButton => driver.FindElement(By.Id("register"));
+        public IWebElement ReturnLoginButton => driver.FindElement(By.XPath("//div[contains(@class, 'login link btn btn-link')]"));
+
+        public void Registration(string login, string email, string password, string confirmpassword)
+        {
+            LoginField.SendKeys(login);
+            EmailField.SendKeys(email);
+            PasswordField.SendKeys(password);
+            ConfirmField.SendKeys(confirmpassword);
+            RegisterButton.Click();
+        }
+
+    }
+}
---
+++ Test2/Tests/RegisterPageTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test2.Pages;
+
+namespace Test2.Tests
+{
+    internal class RegisterPageTest : BaseTest
+    {
+        [SetUp]
+        public void OpenRegister()
+        {
+            OpenDriver();
+            driver.Url = "https://localhost:5001/Register";
+        }
+
+        [TestCase("test", "test@test.com", "newyork1", "newyork1")]
+        public void RegisterPositive(string login, string email, string password, string cConfirmpassword)
+        {
+            RegisterPage registerPage = new RegisterPage(driver);
+            CalculatorPage calculatorPage = new CalculatorPage(driver);
+
+            registerPage.Registration(login, email, password, cConfirmpassword);
+            Assert.IsTrue(calculatorPage.IsOpened);
+        }       
+    }
+}
